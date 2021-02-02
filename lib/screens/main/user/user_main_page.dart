1ac30b3eea78cb8051bb209026bc91c7e85e3ab3
wptan <wptan9915@gmail.com>
2021-02-02 16:57:59 +0800

    Rename from user_page.dart

--- lib/screens/main/user/user_main_page.dart
+++ lib/screens/main/user/user_main_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:bordered_text/bordered_text.dart';
-import 'package:waste_management/ui/curve_painter.dart';
+import 'package:waste_management/widgets/curve_painter.dart';
+import 'package:waste_management/screens/main/user/user_register_complaints.dart';
+import 'package:waste_management/widgets/custom_decoration.dart';
+import 'package:waste_management/widgets/icon_and_title.dart';
 
 class UserMainPage extends StatelessWidget {
   @override
@@ -17,22 +19,19 @@ class UserMainPage extends StatelessWidget {
         ),
         Container(
           margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                blurRadius: 5,
-                offset: Offset(5, 5), // changes position of shadow
-              ),
-            ],
-          ),
+          decoration: mainButtonBoxDecoration,
           child: FlatButton(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10.0),
             ),
             minWidth: 220,
             height: 80,
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UserRegisterComplaints()),
+              );
+            },
             color: Colors.white,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -59,15 +58,7 @@ class UserMainPage extends StatelessWidget {
         ),
         Container(
           margin: EdgeInsets.fromLTRB(50, 30, 50, 0),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                blurRadius: 5,
-                offset: Offset(5, 5), // changes position of shadow
-              ),
-            ],
-          ),
+          decoration: mainButtonBoxDecoration,
           child: FlatButton(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10.0),
@@ -101,15 +92,7 @@ class UserMainPage extends StatelessWidget {
         ),
         Container(
           margin: EdgeInsets.fromLTRB(50, 30, 50, 10),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                blurRadius: 5,
-                offset: Offset(5, 5), // changes position of shadow
-              ),
-            ],
-          ),
+          decoration: mainButtonBoxDecoration,
           child: FlatButton(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10.0),
@@ -143,52 +126,16 @@ class UserMainPage extends StatelessWidget {
         ),
       ],
     );
+
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
         child: Stack(
           children: [
-            Positioned.fill(
-                child: CustomPaint(
-              painter: CurvePainter(),
-            )),
+            BackgroundPainter(),
             Column(
               children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 50, 20, 0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: Image.asset(
-                          "assets/icon/apps_icon.png",
-                        ),
-                      ),
-                      Expanded(
-                        flex: 6,
-                        child: BorderedText(
-                          strokeWidth: 0.5,
-                          child: Text(
-                              "Solid Waste Management",
-                              style: TextStyle(
-                                  decorationColor: Colors.black,
-                                  shadows: [
-                                    Shadow(
-                                      blurRadius: 10.0,
-                                      color: Colors.grey.withOpacity(0.5),
-                                      offset: Offset(5, 5),
-                                    ),
-                                  ],
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 30 * screenWidth / demoWidth
-                              )
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                IconAndTitle(screenWidth: screenWidth, demoWidth: demoWidth,),
                 Column(
                   children: <Widget>[
                     Container(
@@ -212,25 +159,23 @@ class UserMainPage extends StatelessWidget {
           ),
         ),
         child: BottomNavigationBar(
-          onTap: (index) {},
           backgroundColor: Colors.transparent,
           type: BottomNavigationBarType.fixed,
           elevation: 0,
           unselectedItemColor: Colors.white,
-          selectedIconTheme: IconThemeData(color: Colors.white),
+          selectedItemColor: Colors.amberAccent,
+          currentIndex: 0,
           items: [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               title: Text(
                 "Home",
-                style: TextStyle(color: Colors.white),
               ),
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.logout),
               title: Text(
                 "Log Out",
-                style: TextStyle(color: Colors.white),
               ),
             ),
           ],
