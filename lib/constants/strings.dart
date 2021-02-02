f5b0681b994fb9ce8503323d3edfae6b3ed1ab9e
wptan <wptan9915@gmail.com>
2021-02-02 19:47:28 +0800

    Add more string

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -5,10 +5,28 @@ const sPassword = "Password";
 const sConfirmPassword = "Confirm Password";
 const sLogin = "Login";
 const sSignUp = "Sign Up";
+const sHome = "Home";
+const sLogOut = "Log Out";
 
 // User
 const sUserLogin = "User Login";
 const sUserRegister = "User Register";
+const sRegisterComplaints = "Register Complaints";
+//// Register Complaints
+const sTicket = "Ticket";
+const sBinID = "Bin ID:";
+const sArea = "Area:";
+const sLocality = "Locality:";
+const sLandMark = "Landmark:";
+const sCity = "City:";
+const sPostComplaint = "Post Complaint";
+const sMessage = "Message:";
+const sDescription = "Description";
+const sSubmit = "Submit";
+//// My Complaints
+const sMyComplaints = "My Complaints";
+//// My Profile
+const sMyProfile = "My Profile";
 
 // Admin
 const sAdminLogin = "Admin Login";
