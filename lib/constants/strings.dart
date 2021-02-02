ea40e709997a85aaeb9d200fcf940a7208f82a3f
wptan <wptan9915@gmail.com>
2021-02-02 19:26:37 +0800

    Add strings for easier maintenance

---
+++ lib/constants/strings.dart
@@ -0,0 +1,14 @@
+// common
+const sAppTitle = "Solid Waste Management";
+const sUsername = "Username";
+const sPassword = "Password";
+const sConfirmPassword = "Confirm Password";
+const sLogin = "Login";
+const sSignUp = "Sign Up";
+
+// User
+const sUserLogin = "User Login";
+const sUserRegister = "User Register";
+
+// Admin
+const sAdminLogin = "Admin Login";
