5a5a04fa105ee3a003946be9ae77ab63b5a17af1
wptan <wptan9915@gmail.com>
2021-02-03 15:46:45 +0800

    Add more strings

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -11,22 +11,40 @@ const sLogOut = "Log Out";
 // User
 const sUserLogin = "User Login";
 const sUserRegister = "User Register";
+const sUserDetail = "User Detail";
 const sRegisterComplaints = "Register Complaints";
 //// Register Complaints
 const sTicket = "Ticket";
-const sBinID = "Bin ID:";
-const sArea = "Area:";
-const sLocality = "Locality:";
-const sLandMark = "Landmark:";
-const sCity = "City:";
+const sBinID = "Bin ID";
+const sFTState = "FT/State";
+const sCapital = "Capital";
+const sCity = "City";
+const sArea = "Area";
 const sPostComplaint = "Post Complaint";
-const sMessage = "Message:";
+const sMessage = "Message";
 const sDescription = "Description";
+const sContinue = "Continue";
+const sConfirm = "Confirm?";
+const sNoDescription = "-";
 const sSubmit = "Submit";
+const sCancel = "Cancel";
+const sSubmitSuccessfully = "Submit Successfully";
+const sComplaintID = "Complaint ID";
+const sOk = "Ok";
 //// My Complaints
 const sMyComplaints = "My Complaints";
+const sStatus = "Status";
+const sPending = "Pending";
+const sCompleted = "Completed";
 //// My Profile
 const sMyProfile = "My Profile";
+const sName = "Name";
+const sEmail = "Email";
+const sAddress = "Address";
+const sUserID = "User ID";
+const sEditProfile = "Edit Profile";
+const sSave = "Save";
+const sSaveSuccessfully = "Save Successfully";
 
 // Admin
 const sAdminLogin = "Admin Login";
