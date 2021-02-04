509d0d7487a23fe44aec0e174d9ff2a5e36db710
wptan <wptan9915@gmail.com>
2021-02-05 01:24:41 +0800

    Add Bin class to store data about bin

---
+++ lib/data/bin.dart
@@ -0,0 +1,11 @@
+class Bin {
+  // Example: B00001
+  final String binID;
+  final String ftState;
+  final String district;
+  final String subDistrict;
+  final String area;
+  final String cleaningPeriod;
+
+  Bin(this.binID, this.ftState, this.district, this.subDistrict, this.area, this.cleaningPeriod);
+}
