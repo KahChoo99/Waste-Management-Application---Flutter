6ee9f4b08bf570ec771d535e69ec8ac9fe613b47
wptan <wptan9915@gmail.com>
2021-02-06 01:14:32 +0800

    Change main name structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:waste_management/data/bin/bin.dart';
+import 'package:waste_management/data/complaint/complaint.dart';
+import 'package:waste_management/data/data.dart';
+import 'package:waste_management/data/user/user.dart';
+import 'package:waste_management/data/userProfile/userProfile.dart';
 import 'package:waste_management/screens/login_register/login_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
+  Hive.registerAdapter(BinAdapter());
+  Hive.registerAdapter(ComplaintAdapter());
+  Hive.registerAdapter(UserAdapter());
+  Hive.registerAdapter(UserProfileAdapter());
+  await Data.getInstance().init();
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
       .then((_) {
     runApp(
-        MyAdaptingApp());
+        MyApp());
   });
 }
 
-class MyAdaptingApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
@@ -32,18 +43,7 @@ class MyAdaptingApp extends StatelessWidget {
           child: Material(child: child),
         );
       },
-      home: FutureBuilder(
-        future: Hive.openBox('currentUser'),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
-          if (snapshot.connectionState == ConnectionState.done){
-            if (snapshot.hasError)
-              return Text(snapshot.error.toString());
-            else
-              return LoginRegister();
-          } else
-            return Scaffold();
-        },
-      ),
+      home: LoginRegister()
     );
   }
 }
