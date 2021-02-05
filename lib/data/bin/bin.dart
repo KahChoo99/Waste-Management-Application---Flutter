3c516a86bd4383db10ef4206643c296653ea2b74
wptan <wptan9915@gmail.com>
2021-02-06 01:12:16 +0800

    Add Bin Hive Adapter

--- lib/data/bin/bin.dart
+++ lib/data/bin/bin.dart
@@ -1,10 +1,26 @@
+import 'package:hive/hive.dart';
+
+part 'bin.g.dart';
+
+@HiveType(typeId: 0)
 class Bin {
   // Example: B00001
+  @HiveField(0)
   final String binID;
+
+  @HiveField(1)
   final String ftState;
+
+  @HiveField(2)
   final String district;
+
+  @HiveField(3)
   final String subDistrict;
+
+  @HiveField(4)
   final String area;
+
+  @HiveField(5)
   final String cleaningPeriod;
 
   Bin(this.binID, this.ftState, this.district, this.subDistrict, this.area, this.cleaningPeriod);
