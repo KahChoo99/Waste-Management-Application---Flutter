437e6ffade5579e6c249476d1d6f714958beef78
wptan <wptan9915@gmail.com>
2021-02-06 19:13:29 +0800

    Add get data as Map in complaint.dart

--- lib/data/complaint/complaint.dart
+++ lib/data/complaint/complaint.dart
@@ -1,4 +1,5 @@
 import 'package:hive/hive.dart';
+import 'package:waste_management/constants/strings.dart';
 import 'package:waste_management/data/bin/bin.dart';
 
 part 'complaint.g.dart';
@@ -22,7 +23,33 @@ class Complaint {
   String commentMessage;
 
   @HiveField(5)
-  final String status;
+  String status;
 
   Complaint(this.complaintID, this.userID, this.bin, this.complaintMessage, this.commentMessage, this.status);
+
+  Map<String, String> getBinDataForUser() {
+    return {
+      sComplaintID: this.complaintID,
+      sBinID: this.bin.binID,
+      sFTState: this.bin.fTState,
+      sDistrict: this.bin.district,
+      sSubDistrict: this.bin.subDistrict,
+      sArea: this.bin.area,
+      sCleaningPeriod: this.bin.cleaningPeriod,
+    };
+  }
+
+  Map<String, String> getBinDataForAdmin() {
+    return {
+      sComplaintID: this.complaintID,
+      sUserID: this.userID,
+      sBinID: this.bin.binID,
+      sFTState: this.bin.fTState,
+      sDistrict: this.bin.district,
+      sSubDistrict: this.bin.subDistrict,
+      sArea: this.bin.area,
+      sCleaningPeriod: this.bin.cleaningPeriod,
+      sStatus: this.status
+    };
+  }
 }
