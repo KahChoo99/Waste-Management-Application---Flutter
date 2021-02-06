5e8dcc3ff81c2a37e5d8c381428c825bb497fc33
wptan <wptan9915@gmail.com>
2021-02-07 01:15:24 +0800

    Set responsive layout for user_my_complaints_status.dart.dart

--- lib/screens/main/user/user_my_complaints_status.dart
+++ lib/screens/main/user/user_my_complaints_status.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:waste_management/constants/strings.dart';
 import 'package:waste_management/constants/themes.dart';
+import 'package:waste_management/constants/values.dart';
 import 'package:waste_management/data/complaint/complaint.dart';
 import 'package:waste_management/widgets/arrow_back_pop.dart';
 import 'package:waste_management/widgets/curve_painter.dart';
@@ -24,6 +25,9 @@ class _UserMyComplaintsStatus extends State<UserMyComplaintsStatus> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
+    double widthRatio = screenWidth / dDemoWidth;
+    double heightRatio = screenHeight / dDemoHeight;
+
     Map<String, String> binComplaintData = widget.complaint.getBinDataForUser();
     List<String> binKeys = binComplaintData.keys.toList();
 
@@ -33,23 +37,37 @@ class _UserMyComplaintsStatus extends State<UserMyComplaintsStatus> {
     Column cardList = Column(
       children: [
         SizedBox(
-          height: 20,
+          height: 20 * heightRatio,
         ),
         Container(
           width: screenWidth,
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-          margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          padding: EdgeInsets.fromLTRB(
+            10 * widthRatio,
+            10 * heightRatio,
+            10 * widthRatio,
+            10 * heightRatio,
+          ),
+          margin: EdgeInsets.fromLTRB(
+            30 * widthRatio,
+            0,
+            30 * widthRatio,
+            0,
+          ),
           decoration: mainContainerBGBoxDecoration,
           child: Column(
             children: [
               Align(
                 alignment: Alignment.centerLeft,
-                child: Text(sPostComplaint,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                child: Text(
+                  sPostComplaint,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24 * widthRatio,
+                  ),
+                ),
               ),
               SizedBox(
-                height: 10,
+                height: 10 * heightRatio,
               ),
               for (String binKey in binKeys)
                 Row(
@@ -58,25 +76,47 @@ class _UserMyComplaintsStatus extends State<UserMyComplaintsStatus> {
                     Expanded(
                       flex: 4,
                       child: Container(
-                        padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                        margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                        child: Text(binKey,
-                            style: TextStyle(
-                                color: wordAndIconBlue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16)),
+                        padding: EdgeInsets.fromLTRB(
+                          0,
+                          2 * heightRatio,
+                          0,
+                          0,
+                        ),
+                        margin: EdgeInsets.fromLTRB(
+                          0,
+                          0,
+                          0,
+                          10 * heightRatio,
+                        ),
+                        child: Text(
+                          binKey,
+                          style: TextStyle(
+                            color: wordAndIconBlue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16 * widthRatio,
+                          ),
+                        ),
                       ),
                     ),
                     SizedBox(
-                      width: 10,
+                      width: 10 * widthRatio,
                     ),
                     Expanded(
                       flex: 6,
                       child: Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                        child: Text(binComplaintData[binKey],
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18)),
+                        margin: EdgeInsets.fromLTRB(
+                          0,
+                          0,
+                          0,
+                          10 * heightRatio,
+                        ),
+                        child: Text(
+                          binComplaintData[binKey],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18 * widthRatio,
+                          ),
+                        ),
                       ),
                     )
                   ],
@@ -84,57 +124,104 @@ class _UserMyComplaintsStatus extends State<UserMyComplaintsStatus> {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: Text(sMessage,
-                      style: TextStyle(
-                          color: wordAndIconBlue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16)),
+                  padding: EdgeInsets.fromLTRB(0, 2 * heightRatio, 0, 0),
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10 * heightRatio),
+                  child: Text(
+                    sMessage,
+                    style: TextStyle(
+                      color: wordAndIconBlue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16 * widthRatio,
+                    ),
+                  ),
                 ),
               ),
               Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(complaintMessage,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18)),
-                  )),
+                margin: EdgeInsets.fromLTRB(
+                  0,
+                  0,
+                  0,
+                  10 * heightRatio,
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    complaintMessage,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18 * widthRatio,
+                    ),
+                  ),
+                ),
+              ),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: Text(sComment,
-                      style: TextStyle(
-                          color: wordAndIconBlue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16)),
+                  padding: EdgeInsets.fromLTRB(
+                    0,
+                    2 * heightRatio,
+                    0,
+                    0,
+                  ),
+                  margin: EdgeInsets.fromLTRB(
+                    0,
+                    0,
+                    0,
+                    10 * heightRatio,
+                  ),
+                  child: Text(
+                    sComment,
+                    style: TextStyle(
+                      color: wordAndIconBlue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16 * widthRatio,
+                    ),
+                  ),
                 ),
               ),
               Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(commentMessage,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18)),
-                  )),
+                margin: EdgeInsets.fromLTRB(
+                  0,
+                  0,
+                  0,
+                  10 * heightRatio,
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    commentMessage,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18 * widthRatio,
+                    ),
+                  ),
+                ),
+              ),
               Container(
-                margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                margin: EdgeInsets.fromLTRB(
+                  50 * widthRatio,
+                  10 * heightRatio,
+                  50 * widthRatio,
+                  10 * heightRatio,
+                ),
                 decoration: mainButtonBoxDecoration,
                 child: FlatButton(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                    borderRadius: BorderRadius.circular(10.0 * heightRatio),
                   ),
-                  minWidth: 220,
-                  height: 50,
+                  minWidth: 220 * widthRatio,
+                  height: 50 * heightRatio,
                   onPressed: () {},
-                  color: widget.complaint.status == sPending ? buttonBlue : buttonGreen,
-                  child: Text(widget.complaint.status,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  color: widget.complaint.status == sPending
+                      ? buttonBlue
+                      : buttonGreen,
+                  child: Text(
+                    widget.complaint.status,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24 * widthRatio,
+                    ),
+                  ),
                   textColor: Colors.black,
                 ),
               ),
@@ -154,7 +241,7 @@ class _UserMyComplaintsStatus extends State<UserMyComplaintsStatus> {
               child: Column(
                 children: [
                   SizedBox(
-                    height: 50,
+                    height: 50 * heightRatio,
                   ),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -170,9 +257,12 @@ class _UserMyComplaintsStatus extends State<UserMyComplaintsStatus> {
               ),
             ),
           ),
-          ArrowBackPop(screenWidth: screenWidth, screenHeight: screenHeight),
+          ArrowBackPop(
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
+          ),
         ],
-      )
+      ),
     );
   }
 }
