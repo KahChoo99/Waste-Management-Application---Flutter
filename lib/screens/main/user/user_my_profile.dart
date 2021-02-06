5e6845159370223f96b7666eb842d844963f8d14
wptan <wptan9915@gmail.com>
2021-02-07 01:09:37 +0800

    Set responsive layout for user_my_profile.dart

--- lib/screens/main/user/user_my_profile.dart
+++ lib/screens/main/user/user_my_profile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:waste_management/constants/strings.dart';
 import 'package:waste_management/constants/themes.dart';
+import 'package:waste_management/constants/values.dart';
 import 'package:waste_management/data/data.dart';
 import 'package:waste_management/data/userProfile/userProfile.dart';
 import 'package:waste_management/screens/main/user/user_edit_my_profile.dart';
@@ -24,31 +25,44 @@ class _UserMyProfile extends State<UserMyProfile> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
+    double widthRatio = screenWidth / dDemoWidth;
+    double heightRatio = screenHeight / dDemoHeight;
+
     UserProfile userProfile = d.userProfile;
     Map<String, String> userData = userProfile.getUseProfileData();
 
     Column cardList = Column(
       children: [
         SizedBox(
-          height: 20,
+          height: 20 * heightRatio,
         ),
         Container(
           width: screenWidth,
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-          margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          padding: EdgeInsets.fromLTRB(
+            10 * widthRatio,
+            10 * heightRatio,
+            10 * widthRatio,
+            10 * heightRatio,
+          ),
+          margin: EdgeInsets.fromLTRB(
+            30 * widthRatio,
+            0,
+            30 * widthRatio,
+            0,
+          ),
           decoration: mainContainerBGBoxDecoration,
           child: Column(
             children: [
               SizedBox(
-                height: 10,
+                height: 10 * heightRatio,
               ),
               Icon(
                 Icons.account_circle_sharp,
                 color: wordAndIconBlue,
-                size: 100,
+                size: 100 * heightRatio,
               ),
               SizedBox(
-                height: 20,
+                height: 20 * heightRatio,
               ),
               for (String userDataKey in userData.keys.toList())
                 Row(
@@ -57,91 +71,135 @@ class _UserMyProfile extends State<UserMyProfile> {
                     Expanded(
                       flex: 3,
                       child: Container(
-                        margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
-                        child: Text(userDataKey,
-                            style: TextStyle(
-                                color: wordAndIconBlue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20)),
+                        margin: EdgeInsets.fromLTRB(
+                          10 * widthRatio,
+                          0,
+                          0,
+                          10 * heightRatio,
+                        ),
+                        child: Text(
+                          userDataKey,
+                          style: TextStyle(
+                            color: wordAndIconBlue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20 * widthRatio,
+                          ),
+                        ),
                       ),
                     ),
                     SizedBox(
-                      width: 15,
+                      width: 15 * widthRatio,
                     ),
                     Expanded(
                       flex: 7,
                       child: Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 10, 10),
-                        child: Text(userData[userDataKey],
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20)),
+                        margin: EdgeInsets.fromLTRB(
+                          0,
+                          0,
+                          10 * widthRatio,
+                          10 * heightRatio,
+                        ),
+                        child: Text(
+                          userData[userDataKey],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20 * widthRatio,
+                          ),
+                        ),
                       ),
                     )
                   ],
                 ),
               SizedBox(
-                height: 10,
+                height: 10 * heightRatio,
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                margin: EdgeInsets.fromLTRB(
+                  50 * widthRatio,
+                  10 * heightRatio,
+                  50 * widthRatio,
+                  10 * heightRatio,
+                ),
                 decoration: mainButtonBoxDecoration,
                 child: FlatButton(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                    borderRadius: BorderRadius.circular(10.0 * heightRatio),
                   ),
-                  minWidth: 220,
-                  height: 50,
+                  minWidth: 220 * widthRatio,
+                  height: 50 * heightRatio,
                   // onPressed: () => showConfirmSubmit(context, binData, _messageEditingController),
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => UserEditMyProfile(userProfile: userProfile,)),
+                        builder: (context) => UserEditMyProfile(
+                          userProfile: userProfile,
+                        ),
+                      ),
                     );
                   },
                   color: buttonBlue,
-                  child: Text(sEditProfile,
-                      style:
-                      TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  child: Text(
+                    sEditProfile,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24 * widthRatio,
+                    ),
+                  ),
                   textColor: Colors.black,
                 ),
               ),
             ],
           ),
         ),
+        SizedBox(
+          height: 30 * heightRatio,
+        ),
       ],
     );
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.center,
-          child: Stack(
-            children: [
-              BackgroundPainter(),
-              Column(
+      body: Stack(
+        children: [
+          BackgroundPainter(),
+          SingleChildScrollView(
+            child: Container(
+              alignment: Alignment.center,
+              child: Stack(
                 children: [
-                  SizedBox(
-                    height: 50,
-                  ),
                   Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      IconAndTitle(screenWidth: screenWidth),
-                      Text(sMyProfile,
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-                      Container(
-                        width: screenWidth,
-                        child: cardList,
+                    children: [
+                      SizedBox(
+                        height: 50 * heightRatio,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          IconAndTitle(screenWidth: screenWidth),
+                          Text(
+                            sMyProfile,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24 * widthRatio,
+                            ),
+                          ),
+                          Container(
+                            width: screenWidth,
+                            child: cardList,
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                  ArrowBackPop(
+                    screenWidth: screenWidth,
+                    screenHeight: screenHeight,
+                  ),
                 ],
               ),
-              ArrowBackPop(screenWidth: screenWidth, screenHeight: screenHeight),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
