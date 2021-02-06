5fd346f8ad651d68b40e5666844d03d69fefb647
wptan <wptan9915@gmail.com>
2021-02-06 23:28:15 +0800

    Set responsive layout for user_register_detail.dart

--- lib/screens/login_register/user/user_register_detail.dart
+++ lib/screens/login_register/user/user_register_detail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:waste_management/constants/strings.dart';
 import 'package:waste_management/constants/themes.dart';
+import 'package:waste_management/constants/values.dart';
 import 'package:waste_management/data/data.dart';
 import 'package:waste_management/data/user/user.dart';
 import 'package:waste_management/data/userProfile/userProfile.dart';
@@ -36,33 +37,62 @@ class _UserRegisterDetail extends State<UserRegisterDetail> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
+    double widthRatio = screenWidth / dDemoWidth;
+    double heightRatio = screenHeight / dDemoHeight;
+
     Column registerPart = Column(
       children: [
         SizedBox(
-          height: 30,
+          height: 30 * heightRatio,
         ),
-        Text(sUserDetail,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+        Text(
+          sUserDetail,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24 * widthRatio,
+          ),
+        ),
         Container(
-          margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+          margin: EdgeInsets.fromLTRB(
+            30 * widthRatio,
+            0,
+            30 * widthRatio,
+            10 * heightRatio,
+          ),
           child: TextField(
             controller: _nameEditingController,
             decoration: InputDecoration(
               labelText: sName,
             ),
+            style: TextStyle(
+              fontSize: (defaultTextFieldFontSize + 2) * widthRatio,
+            ),
           ),
         ),
         Container(
-          margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+          margin: EdgeInsets.fromLTRB(
+            30 * widthRatio,
+            0,
+            30 * widthRatio,
+            10 * heightRatio,
+          ),
           child: TextField(
             controller: _emailEditingController,
             decoration: InputDecoration(
               labelText: sEmail,
             ),
+            style: TextStyle(
+              fontSize: (defaultTextFieldFontSize + 2) * widthRatio,
+            ),
           ),
         ),
         Container(
-          margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
+          margin: EdgeInsets.fromLTRB(
+            30 * widthRatio,
+            0,
+            30 * widthRatio,
+            10 * heightRatio,
+          ),
           child: TextField(
             keyboardType: TextInputType.multiline,
             maxLines: null,
@@ -72,17 +102,25 @@ class _UserRegisterDetail extends State<UserRegisterDetail> {
             decoration: InputDecoration(
               labelText: sAddress,
             ),
+            style: TextStyle(
+              fontSize: (defaultTextFieldFontSize + 2) * widthRatio,
+            ),
           ),
         ),
         Container(
-          margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+          margin: EdgeInsets.fromLTRB(
+            50 * widthRatio,
+            10 * heightRatio,
+            50 * widthRatio,
+            10 * heightRatio,
+          ),
           decoration: mainButtonBoxDecoration,
           child: FlatButton(
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
+              borderRadius: BorderRadius.circular(10.0 * heightRatio),
             ),
-            minWidth: 150,
-            height: 50,
+            minWidth: 150 * widthRatio,
+            height: 50 * heightRatio,
             onPressed: () {
               if (_nameEditingController.text.isEmpty ||
                   _emailEditingController.text.isEmpty ||
@@ -101,20 +139,25 @@ class _UserRegisterDetail extends State<UserRegisterDetail> {
                   print("Name = " + name);
                   print("Email = " + email);
                   print("Address = " + address);
-                  UserProfile userProfile = UserProfile(widget.user, userID, name, email, address);
+                  UserProfile userProfile =
+                      UserProfile(widget.user, userID, name, email, address);
                   d.addNewUser(userProfile);
                   showRegisterUserSuccess(context);
                 }
               }
             },
             color: buttonGreen,
-            child: Text(sSignUp,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-            // textColor: Colors.black,
+            child: Text(
+              sSignUp,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24 * widthRatio,
+              ),
+            ),
           ),
         ),
         SizedBox(
-          height: 60,
+          height: 20 * heightRatio,
         ),
       ],
     );
@@ -129,25 +172,43 @@ class _UserRegisterDetail extends State<UserRegisterDetail> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+                  SizedBox(
+                    height: 50 * heightRatio,
+                  ),
                   Image.asset(
                     "assets/icon/apps_icon.png",
-                    height: 200,
+                    height: 200 * heightRatio,
                   ),
-                  Text(sAppTitle,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 24)),
+                  Text(
+                    sAppTitle,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24 * widthRatio,
+                    ),
+                  ),
                   SizedBox(
-                    height: 16,
+                    height: 16 * heightRatio,
                   ),
                   Container(
                     width: screenWidth,
-                    margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                    margin: EdgeInsets.fromLTRB(
+                      50 * widthRatio,
+                      0,
+                      50 * widthRatio,
+                      0,
+                    ),
                     decoration: mainContainerBGBoxDecoration,
                     child: registerPart,
                   ),
+                  SizedBox(
+                    height: 30 * heightRatio,
+                  ),
                 ],
               ),
-              ArrowBackPop(screenWidth: screenWidth, screenHeight: screenHeight),
+              ArrowBackPop(
+                screenWidth: screenWidth,
+                screenHeight: screenHeight,
+              ),
             ],
           ),
         ),
