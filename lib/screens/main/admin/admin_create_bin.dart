f87266a025f6f3ddf62da7273bd35aaa1bbdee34
wptan <wptan9915@gmail.com>
2021-02-06 23:44:50 +0800

    Set responsive layout for admin_create_bin.dart

--- lib/screens/main/admin/admin_create_bin.dart
+++ lib/screens/main/admin/admin_create_bin.dart
@@ -48,6 +48,9 @@ class _AdminCreateBin extends State<AdminCreateBin> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
+    double widthRatio = screenWidth / dDemoWidth;
+    double heightRatio = screenHeight / dDemoHeight;
+
     if (fTState != null) {
       if (district == null) {
         districtList =
@@ -65,34 +68,59 @@ class _AdminCreateBin extends State<AdminCreateBin> {
     Column cardList = Column(
       children: [
         SizedBox(
-          height: 10 * screenHeight / dDemoHeight,
+          height: 10 * heightRatio,
         ),
         Container(
           width: screenWidth,
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-          margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          padding: EdgeInsets.fromLTRB(
+            10 * widthRatio,
+            10 * heightRatio,
+            10 * widthRatio,
+            10 * heightRatio,
+          ),
+          margin: EdgeInsets.fromLTRB(
+            30 * widthRatio,
+            0,
+            30 * widthRatio,
+            0,
+          ),
           decoration: mainContainerBGBoxDecoration,
           child: Column(
             children: [
               SizedBox(
-                height: 10,
+                height: 10 * heightRatio,
               ),
               for (var i = 0; i < allDropDownList.length; i++)
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      child: Text(columnType[i],
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16)),
+                      margin: EdgeInsets.fromLTRB(
+                        10 * widthRatio,
+                        0,
+                        10 * widthRatio,
+                        0,
+                      ),
+                      child: Text(
+                        columnType[i],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16 * widthRatio,
+                        ),
+                      ),
                     ),
                     Container(
                         width: screenWidth,
-                        height: 50,
-                        margin: EdgeInsets.fromLTRB(10, 0, 10, 15),
+                        height: 50 * heightRatio,
+                        margin: EdgeInsets.fromLTRB(
+                          10 * widthRatio,
+                          0,
+                          10 * widthRatio,
+                          15 * heightRatio,
+                        ),
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
+                            borderRadius:
+                                BorderRadius.circular(10.0 * heightRatio),
                             border: Border.all()),
                         child: DropdownButtonHideUnderline(
                           child: ButtonTheme(
@@ -106,8 +134,13 @@ class _AdminCreateBin extends State<AdminCreateBin> {
                                       (String value) {
                                 return DropdownMenuItem<String>(
                                   value: value,
-                                  child: Text(value,
-                                      style: TextStyle(fontSize: 16)),
+                                  child: Text(
+                                    value,
+                                    style: TextStyle(
+                                      fontSize: (defaultTextFieldFontSize + 2) *
+                                          widthRatio,
+                                    ),
+                                  ),
                                 );
                               }).toList(),
                               onChanged: (String value) {
@@ -148,13 +181,27 @@ class _AdminCreateBin extends State<AdminCreateBin> {
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                 Container(
                   width: screenWidth,
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Text(sArea,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  margin: EdgeInsets.fromLTRB(
+                    10 * widthRatio,
+                    0,
+                    10 * widthRatio,
+                    0,
+                  ),
+                  child: Text(
+                    sArea,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16 * widthRatio,
+                    ),
+                  ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  margin: EdgeInsets.fromLTRB(
+                    10 * widthRatio,
+                    0,
+                    10 * widthRatio,
+                    0,
+                  ),
                   child: TextField(
                     maxLines: 2,
                     maxLength: 50,
@@ -163,12 +210,15 @@ class _AdminCreateBin extends State<AdminCreateBin> {
                     decoration: InputDecoration(
                       hintText: sDescription,
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                        borderRadius: BorderRadius.circular(10.0 * heightRatio),
                         borderSide: BorderSide(
                           color: Colors.amber,
                           style: BorderStyle.solid,
                         ),
                       ),
+                    ),
+                    style: TextStyle(
+                      fontSize: (defaultTextFieldFontSize + 2) * widthRatio,
                     ),
                   ),
                 ),
@@ -176,13 +226,27 @@ class _AdminCreateBin extends State<AdminCreateBin> {
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                 Container(
                   width: screenWidth,
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Text("$sCleaningPeriod (days/per week)",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  margin: EdgeInsets.fromLTRB(
+                    10 * widthRatio,
+                    0,
+                    10 * widthRatio,
+                    0,
+                  ),
+                  child: Text(
+                    "$sCleaningPeriod (days/per week)",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16 * widthRatio,
+                    ),
+                  ),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  margin: EdgeInsets.fromLTRB(
+                    10 * widthRatio,
+                    0,
+                    10 * widthRatio,
+                    0,
+                  ),
                   child: TextField(
                     keyboardType: TextInputType.number,
                     maxLines: 1,
@@ -192,28 +256,36 @@ class _AdminCreateBin extends State<AdminCreateBin> {
                     decoration: InputDecoration(
                       hintText: sHowManyDaysPerWeek,
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                        borderRadius: BorderRadius.circular(10.0 * heightRatio),
                         borderSide: BorderSide(
                           color: Colors.amber,
                           style: BorderStyle.solid,
                         ),
                       ),
                     ),
+                    style: TextStyle(
+                      fontSize: (defaultTextFieldFontSize + 2) * widthRatio,
+                    ),
                   ),
                 ),
               ]),
               SizedBox(
-                height: 10,
+                height: 10 * heightRatio,
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                margin: EdgeInsets.fromLTRB(
+                  50 * widthRatio,
+                  10 * heightRatio,
+                  50 * widthRatio,
+                  10 * heightRatio,
+                ),
                 decoration: mainButtonBoxDecoration,
                 child: FlatButton(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                    borderRadius: BorderRadius.circular(10.0 * heightRatio),
                   ),
-                  minWidth: 220,
-                  height: 50,
+                  minWidth: 220 * widthRatio,
+                  height: 50 * heightRatio,
                   onPressed: () {
                     if (fTState == null ||
                         district == null ||
@@ -246,9 +318,13 @@ class _AdminCreateBin extends State<AdminCreateBin> {
                     }
                   },
                   color: buttonBlue,
-                  child: Text(sCreate,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  child: Text(
+                    sCreate,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24 * widthRatio,
+                    ),
+                  ),
                   textColor: Colors.black,
                 ),
               ),
@@ -256,7 +332,7 @@ class _AdminCreateBin extends State<AdminCreateBin> {
           ),
         ),
         SizedBox(
-          height: 30,
+          height: 30 * heightRatio,
         ),
       ],
     );
@@ -264,42 +340,48 @@ class _AdminCreateBin extends State<AdminCreateBin> {
     return StatefulBuilder(
       builder: (context, setState) {
         return Scaffold(
-          body: Stack(
-            children: [
-              BackgroundPainter(),
-              SingleChildScrollView(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Stack(
-                    children: [
-                      Column(
-                        children: [
-                          SizedBox(
-                            height: 40,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              IconAndTitle(screenWidth: screenWidth),
-                              Text(sCreateBin,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold, fontSize: 24)),
-                              Container(
-                                width: screenWidth,
-                                child: cardList,
+            body: Stack(
+          children: [
+            BackgroundPainter(),
+            SingleChildScrollView(
+              child: Container(
+                alignment: Alignment.center,
+                child: Stack(
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 40 * heightRatio,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            IconAndTitle(screenWidth: screenWidth),
+                            Text(
+                              sCreateBin,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24 * widthRatio,
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      ArrowBackPop(screenWidth: screenWidth, screenHeight: screenHeight),
-                    ],
-                  ),
+                            ),
+                            Container(
+                              width: screenWidth,
+                              child: cardList,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    ArrowBackPop(
+                      screenWidth: screenWidth,
+                      screenHeight: screenHeight,
+                    ),
+                  ],
                 ),
               ),
-            ],
-          )
-        );
+            ),
+          ],
+        ));
       },
     );
   }
