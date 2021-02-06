9544178635fcd36415f902a8662d6a699f3270bb
wptan <wptan9915@gmail.com>
2021-02-06 21:38:47 +0800

    Add more mechanism in arrow back pop

--- lib/widgets/arrow_back_pop.dart
+++ lib/widgets/arrow_back_pop.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:waste_management/constants/values.dart';
 
 class ArrowBackPop extends StatelessWidget {
+  final double screenWidth;
+  final double screenHeight;
+
+  const ArrowBackPop({this.screenWidth, this.screenHeight});
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => Navigator.of(context).pop(),
       child: Container(
-        width: 80,
-        height: 80,
-        margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+        width: 70 * screenWidth / dDemoWidth,
+        height: 55 * screenHeight / dDemoHeight,
+        margin: EdgeInsets.fromLTRB(0, 40 * screenHeight / dDemoHeight, 0, 0),
         child: IconButton(
           // padding: EdgeInsets.fromLTRB(20, 50, 0, 0),
           icon: Icon(
             Icons.arrow_back_ios_sharp,
             color: Colors.white,
-            size: 40,
+            size: 40 * screenWidth / dDemoWidth,
           ),
           onPressed: () => Navigator.of(context).pop(),
         ),
